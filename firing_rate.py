78311f92cd2dbd1ca1c91aac62b8489fc68891f3
priyap97 <priya.pamnani@rutgers.edu>
2019-02-19 15:42:58 -0500

    support for custom step

--- firing_rate.py
+++ firing_rate.py
@@ -7,12 +7,12 @@ import plotter
 
 def firing_rate(I):
     # Create list of times to run simulation
-    times = [0]
+    times = 0
     dt = 0.1
 
     # Set vars to be used in calculation
     # Variable names equivalent to those used in assignment description
-    Vm = [0.0]  # voltage
+    Vm = 0.0  # voltage
     Cm = 30  # capacitance
     Rm = 7  # resistance
     tau_m = Rm*Cm  # time constant
@@ -22,14 +22,14 @@ def firing_rate(I):
     spikes = 0
 
     # Integrate V from 0.0ms to 1000.0ms
-    while times[-1] < 1000:
-        times.append(round(times[-1] + dt, 1))
-        old_V = Vm[-1]
-        Vm.append(old_V + (((-old_V + I * Rm) / tau_m) * dt))
+    while times < 1000:
+        times = round(times + dt, 1)
+        old_V = Vm
+        Vm = old_V + (((-old_V + I * Rm) / tau_m) * dt)
 
         # Test for spike, if exceeded threshold return to Vr
-        if Vm[-1] >= Vt:
-            Vm[-1] = Vr
+        if Vm >= Vt:
+            Vm = Vr
             spikes = spikes + 1
 
     return spikes
@@ -40,12 +40,17 @@ def simulate():
     min = float(input())
     print('Enter maximum current to measure firing rate')
     max = float(input())
+    print('Enter step (increment) for current. Suggested 0.125')
+    step = float(input())
 
     I = [min]
     f_rate = [firing_rate(min)]
 
     while I[-1] < max:
-        I.append(I[-1] + 0.125)
+        if I[-1] + step <=max:
+            I.append(I[-1] + step)
+        else:
+            I.append(max)
         f_rate.append(firing_rate(I[-1]))
 
     # Plot results
